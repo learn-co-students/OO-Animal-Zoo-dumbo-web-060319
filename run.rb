72fb338f7d5b67081ad399146e3ab3e182998c3d
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-06 15:31:30 -0400

    Done.

--- run.rb
+++ run.rb
@@ -4,6 +4,13 @@ require 'pry'
 
 
 #Test your code here
+zoo1 = Zoo.new("Bronx Zoo", "Bronx")
+zoo2 = Zoo.new("Queens Zoo", "Queens")
+zoo3 = Zoo.new("Brooklyn Zoo", "Brooklyn")
+ 
+animal1 = Animal.new("feline", "5", "kitty", zoo1)
+animal2 = Animal.new("canine", "2", "doggy", zoo2)
+animal3 = Animal.new("bird", "1", "tweety", zoo3)
 
 
 binding.pry

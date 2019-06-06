2394dc6379e7a92a6495ef37cb5ef10b7d185a8b
Alexander Schelchere <ascellie@gmail.com>
2019-06-06 13:27:22 -0400

    IT all works but I have a wierd end thing going on, but this works!

--- run.rb
+++ run.rb
@@ -1,10 +1,15 @@
-require_relative "lib/Animal.rb"
-require_relative "lib/Zoo.rb"
+require_relative "./lib/Animal.rb"
+require_relative "./lib/Zoo.rb"
 require 'pry'
 
 
 #Test your code here
 
+zoo1 = Zoo.new("Bronx Zoo", "New York")
+
+animal1 = Animal.new("Hammy", "Hamadryas Baboon", 48, zoo1)
+animal2 = Animal.new("Snatcher", "Masai Lion", 200, zoo1)
 
 binding.pry
+
 puts "done"

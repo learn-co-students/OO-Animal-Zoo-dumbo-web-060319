e686d6966046824525c6eecde85b7b2fe3d24228
Sean Padden <you@example.com>
2019-06-06 17:36:33 -0400

    Done by Hamza and Sean

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,47 @@
 class Zoo
-
-end
+    attr_reader :location, :name
+ 
+    @@zooinfo = []
+ 
+    def initialize(name, location)
+        @name =  name
+        @location = location
+        @@zooinfo << self
+    end
+ 
+    def self.all
+        @@zooinfo
+    end
+ 
+    def animals
+        # returns all the animals of specific instance of zoo
+        Animal.all
+    end
+ 
+    def animal_species
+        #returns array of all animals (as string) using uinq method
+         Animal.all.map{|as| as.species}.uniq.join(', ')
+    end
+ 
+    def find_by_species(animal_species)
+        al =Animal.all.map{|as| as.species }
+        al.select{|sas| sas == animal_species }
+        #returns array of all animals of animal_species(arg)
+        #[]
+    end
+ 
+    def animal_nickname
+        an = Animal.all.map {|nn| nn.nickname}
+        # all animal nickname of that zoo instance
+        #[]
+    end
+    def find_by_location(arg_location)
+        Zoo.all.select do |l|
+            if l.location == arg_location
+            puts l.name
+            end
+        end
+ 
+    end
+ 
+ end
--- run.rb
+++ run.rb
@@ -4,7 +4,22 @@ require 'pry'
 
 
 #Test your code here
+animals1 = Animal.new("dog" , 10 , "tom1")
+animals2 = Animal.new("cat" , 11 , "tom2")
+animals3 = Animal.new("horse" , 12 , "tom3")
+animals4 = Animal.new("lion" , 13 , "tom4")
+animals5 = Animal.new("lion" , 13 , "tom4")
+animals6 = Animal.new("dog" , 13 , "tom4")
 
+zooinfo1 = Zoo.new("zooname0"  ,"location0")
+zooinfo2 = Zoo.new("zooname1" , "location1" )
+zooinfo3 = Zoo.new("zooname2" , "location1" )
+
+puts zooinfo1.find_by_species("lion")
+
+puts zooinfo3.find_by_location("location1")
+
+puts animals1.find_by_species("dog")
 
 binding.pry
 puts "done"

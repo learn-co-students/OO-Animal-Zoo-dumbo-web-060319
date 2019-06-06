5d5cacbd8d2ae1e39d9642f655662883e5c3c5ea
github username <github email address>
2019-06-06 16:18:09 -0400

    Related Zoos with Animals with Kevin

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,37 @@
 class Zoo
+  attr_accessor :name, :location
+  attr_reader :animals_list
+
+  @@all = []
+
+  #Instance Methods
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def animals
+    Animal.all.select do |animal|
+      animal.zoo == self
+    end
+  end
+
+  def animal_species
+    animals.map { |animal| animal.species }.uniq
+  end
+
+  #Class Methods
+  def self.all
+    @@all
+  end
+
+  def self.find_by_location(location)
+    self.all.select do |l|  # s = location
+      l.location == location
+    end
+
+  end
 
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,16 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
-
+tampa = Zoo.new("Tampa Zoo", "Florida")
+san_diego = Zoo.new("Big Zoo", "San Diego")
+florida = Zoo.new("Little Zoo", "Florida")
+york = Zoo.new("Medium Zoo", "York")
+feline = Animal.new("cat", 35, "Bittles", san_diego)
+tom = Animal.new("cat", 35, "Tom", san_diego)
+carl = Animal.new("cat", 35, "Carl", florida)
+ann = Animal.new("dog", 35, "Ann", florida)
+don = Animal.new("dog", 35, "Don", york)
+steven = Animal.new("dog", 35, "Steven", york)
 
 binding.pry
 puts "done"

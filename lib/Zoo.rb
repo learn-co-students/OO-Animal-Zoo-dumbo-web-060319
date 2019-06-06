e87e2e289283d4c517a34d417d619d58801a3365
hamzaja <hamzajaved71@gmail.com>
2019-06-06 17:35:07 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,23 @@
 class Zoo
+    attr_reader :location, :name 
+    
+    @@zooinfo = []
+    
+    def initialize(name, location)
+        @name =  name 
+        @location = location
+        @@zooinfo << self
+    end
 
+    def self.all
+        @@zooinfo
+    end    
+    def find_by_location(arg_location)
+        self.select do |l| 
+            if l.location == arg_location
+            puts l.name
+            end
+        end
+
+    end
 end

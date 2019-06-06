613e3b8037b7cf0a2f8728c6d7ebfa697e595da0
github username <github email address>
2019-06-06 15:32:30 -0400

    created two class objects that interract

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,52 @@
 class Zoo
+    
+    attr_reader :location, :name
 
+    def initialize (name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+    end
+
+    def animal_species
+        unique_species = []
+        self.animals.each do |animal|
+            if !unique_species.include?(animal.species)
+                unique_species << animal.species
+            end
+        end
+        unique_species
+    end
+
+    def find_by_species(species)
+        self.animals.select do |animal|
+            animal.species == species
+        end
+    end
+
+    def animal_nicknames
+        nicknames_storage = []
+        self.animals.map do |animal|
+            nicknames_storage << animal.nickname
+        end
+        nicknames_storage
+    end
+
+    def self.find_by_location(location)
+        self.all.select do |zoo|
+            zoo.location == location
+        end
+    end
 end

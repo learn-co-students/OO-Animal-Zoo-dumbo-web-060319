e5c22e4f3565363b05394dd59a30860fa6bbf054
Brandon Lau <brandon.lau86@gmail.com>
2019-06-06 15:33:36 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,35 @@
 class Animal
+    attr_reader :weight, :nickname
+    attr_writer :weight
+    attr_accessor :all_animals, :species
+    @@all_animals = []
+
+    def initialize(species, weight, nickname, zoo)
+        @zoo = zoo          #Assigned a zoo!
+        @species = species  #assigned specie
+        @weight = weight 
+        @nickname = nickname    
+        @@all_animals << self   #Put self into global animal
+
+        #REMOVED
+        # @zoo.animals_in_this_zoo << self 
+        
+        
+
+    end 
+
+    def self.all 
+        return @@all_animals
+    end     
+    def zoo 
+        #later
+        return @zoo
+    end 
+    def self.find_by_species(specie)
+        #later
+        @@all_animals.collect do |animal|
+            animal.species == specie 
+        end 
+    end
 
 end
